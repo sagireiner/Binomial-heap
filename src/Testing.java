38d5e75b463459c1dee5be49512f6154fe8e3b48
sagireiner <sagireiner@mail.tau.ac.il>
2023-06-12 14:16:37 +0300

    initialize 12/06

---
+++ src/Testing.java
@@ -0,0 +1,7 @@
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        BinomialHeap heap = new BinomialHeap();
+        heap.insert(1, "1");
+    }
+}
